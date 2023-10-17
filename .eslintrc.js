650091b6168fab01ed110e1c786c6e87c72e4751
Nadiia Tsytsylina <miniova95@gmail.com>
2023-10-17 19:43:42 +0300

    update packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,26 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ['standard', 'prettier'],
-  parserOptions: {
-    ecmaVersion: 12,
-  },
-  rules: {},
-};
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+    }
+}
